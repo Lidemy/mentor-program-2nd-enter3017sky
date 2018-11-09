44882e14ce3d12a6424bf0aa622bb9186599af3d
enter3017sky <enter3017sky@gmail.com>
2018-11-09 18:10:37 +0800

    update week4-fix-m11d09

--- homeworks/week4/hw3/week4_hw3.js
+++ homeworks/week4/hw3/week4_hw3.js
@@ -35,31 +35,28 @@ request.onload = function() {
     if (request.status === 200) {
         // 成功的話 處理資料
         var data = JSON.parse(request.responseText);
-        console.log(data)
-        var cols = data.streams
-        // 有 index 才取得到個別的資料
-        console.log(cols[0])
-        console.log(cols[0].channel.display_name)
+        var cols = data.streams;
+        // console.log(data)
+        // console.log(cols[0]) // 有 index 才取得到個別的資料
+        // console.log(cols[0].channel.display_name)
         // console.log(cols[0].channel.name)
-        console.log(cols[0].preview.medium)
+        // console.log(cols[0].preview.medium)
         // console.log(cols[0].channel.status)
-        // console.log(cols[0].channel.views)
-        console.log(cols[0].channel.logo) 
-        // console.log(cols[0].channel._links.videos) 
-        // console.log(displayName)
+        // // console.log(cols[0].channel.views)
+        // console.log(cols[0].channel.logo) 
 
 
-        var row = document.querySelector('.row')
-        for (var i = 0; i < cols.length; i++) {
-            row.innerHTML +=`
-
+        var row = document.querySelector('.row');
+        var result = '';
+        for (var i = 0; i < cols.length; i++) { // 將內容先處理好
+            result += `
             <div class='row'>
                 <a href="${cols[i].channel.url}" target="_blank">
                     <div class='col'>
                         <div class='preview'>
-                                <img src="${cols[i].preview.medium}" />
-                            </div>
-                            <div class='bottom'>
+                            <img src="${cols[i].preview.medium}" />
+                        </div>
+                        <div class='bottom'>
                             <div class='bottom__avatar'>
                                 <img src="${cols[i].channel.logo}" />
                             </div>
@@ -77,9 +74,9 @@ request.onload = function() {
                 </a>
             </div>`
         }
-
+        row.innerHTML += result;  // 然後在把 result 給 row
     } else {
-        alert('有什麼地方出錯了！！')
+        alert('有什麼地方出錯了！！');
         // We reached our target server, but it returned an error
     }
 };
